c73804811dc79afdca51b5012e53aff6b9e80a62
Gzandonai <giovannizandonai350@gmail.com>
2024-11-04 14:32:04 -0300

    Stylizing  the button style

--- components/buttons.js
+++ components/buttons.js
@@ -3,7 +3,19 @@ import { Text, TouchableOpacity } from "react-native";
 export const Button =() => {
     return (
 
-        <TouchableOpacity>
+        <TouchableOpacity
+        style = {{
+            borderStyle: 'solid',
+            borderWidth: 2,
+            borderRadius: 3,
+            width:'60%' ,
+            height:30,
+            backgroundColor:'#32CD32',
+            opacity: 0.6,
+            alignItems:'center',
+            justifyContent: 'center'
+            
+        }}>
              <Text>Salvar</Text>
 
         </TouchableOpacity>

a0631cb2a4cac81e0a1f6f0d1f36059e3f954a4c
Gzandonai <giovannizandonai350@gmail.com>
2024-11-04 14:15:56 -0300

    Creating a component(Button)

---
+++ components/buttons.js
@@ -0,0 +1,16 @@
+import { Text, TouchableOpacity } from "react-native";
+
+export const Button =() => {
+    return (
+
+        <TouchableOpacity>
+             <Text>Salvar</Text>
+
+        </TouchableOpacity>
+   
+
+
+    )
+
+
+}

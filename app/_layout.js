e23393d10ccbc1803f7f478e9bd4aeb5d1f6dd95
Gzandonai <giovannizandonai350@gmail.com>
2024-11-12 17:33:28 -0300

    styling the tabs, filling icon when user is on that screen

--- app/_layout.js
+++ app/_layout.js
@@ -7,12 +7,17 @@ export default function RootLayout() {
 <Tabs>
     <Tabs.Screen name = 'index'
     options={{
+      headerShown:false,
       headerTitle: 'Home Screen',
       title:'Home Screen',
       tabBarLabelStyle:{fontSize:16, color:'#FA8072'},
-      tabBarIcon: ({color, size}) => (
-        <Ionicons name = 'home-outline' color = '#FA8072' size ={28}/>
-      )
+      tabBarIcon: ({color, size, focused}) => {
+        if(focused){
+          return <Ionicons name = 'home' color = '#FA8072' size ={28} /> 
+          
+    } return <Ionicons name ='home-outline' color = '#FA8072' size ={28} /> 
+    
+    } 
     
       }}/>
 
@@ -20,13 +25,14 @@ export default function RootLayout() {
      options={{headerTitle:'Your List', 
       title:'Your List',
       tabBarLabelStyle: {fontSize:16, color:'#FA8072'},
-      tabBarIcon: ({color, size}) => (
-        <Ionicons name = 'bag-add-outline' color = '#FA8072' size ={28} />
-      )
+      tabBarIcon: ({color, size, focused}) => {
+        if(focused){
+          return <Ionicons name = 'bag-add' color = '#FA8072' size ={28} /> 
+          
+    } return <Ionicons name ='bag-add-outline' color = '#FA8072' size ={28} /> 
+      }
 
-
-
-
+  
      }} />
 
 </Tabs>
